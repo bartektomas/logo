5693c1d8c15745a18da9eb781114727d13fe3e88
Dmitry <df.creative@gmail.com>
2016-09-13 01:10:05 -0400

    Add logger

--- index.js
+++ index.js
@@ -3,24 +3,54 @@
  * There are various "ships" and one canvas
  */
 
-var Component = require('gl-component');
-var spiral = require('spiral-2d');
-var inherits = require('inherits');
-var xtend = require('xtend/mutable');
+let spiral = require('spiral-2d/archimedean');
+require('enable-mobile');
+
+let canvas = document.body.appendChild(document.createElement('canvas'));
+
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
+
+let cx = canvas.width / 2;
+let cy = canvas.height / 2;
+
+let context = canvas.getContext('2d');
 
 
-function MIRCA (opts) {
-	if (!(this instanceof MIRCA)) return new MIRCA(opts);
+context.lineJoin = 'round';
+context.lineCap = 'round';
+context.lineWidth = 8;
+
+let lastX = cx, lastY = cy;
+
+
+
+for (let angle = Math.PI * .0; angle <= Math.PI * 17; angle+=Math.PI/64) {
+	context.beginPath();
+	context.moveTo(lastX, lastY);
+
+	context.lineWidth = (13 + 0*Math.sin(angle/3 + Math.PI*1.666));
+	let r = -Math.PI * (angle + tri(angle * 8) * angle * .02 ) + 0*Math.sin(angle/3);
+
+	let x = cx + -r * Math.cos(angle);
+	let y = cy + r * Math.sin(angle);
+    context.lineTo(x, y);
+	context.stroke();
+	context.closePath();
+
+	lastX = x; lastY = y;
 }
 
-inherits(MIRCA, Component);
+// context.strokeStyle = 'red';
 
-MIRCA.prototype.context = '2d';
 
-MIRCA.prototype.
-
-MIRCA.prototype.draw = function () {
-	this.ships.forEach((ship) => {
-
-	});
+//approx triangle by sines
+function tri (angle) {
+	return 3 * (
+		Math.sin(angle)
+		- 1*Math.sin(3*angle)/9
+		+ 1*Math.sin(5*angle)/25
+		- 1*Math.sin(7*angle)/49
+		+ 1*Math.sin(9*angle)/81
+	) / Math.PI*Math.PI;
 }
